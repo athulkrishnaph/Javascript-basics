383742395777c177bbb73fe14f8d10bc87651237
athulkrishnaph <athulhassi@gmail.com>
2024-11-02 14:01:04 +0530

    commit

--- loops.js
+++ loops.js
@@ -16,3 +16,16 @@ for (let p = 0; p < 3; p++) {
         console.log(`Outer Loop: ${p}, Inner Loop: ${q}`);
     }
 }
+
+const arr = [10, 20, 30, 40, 50]
+
+// for of 
+for (let num of arr) {
+    console.log(num);   
+}
+
+// for in 
+for (let num in arr) {
+    console.log(num,arr[num]);
+    
+}
